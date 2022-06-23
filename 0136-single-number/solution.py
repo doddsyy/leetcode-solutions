49f91d8bdbba7cf8f06a6167fdef7dd0ec454715
doddsyy <88795984+doddsyy@users.noreply.github.com>
2022-06-23 18:11:44 +0000

    Sync LeetCode submission Runtime - 146 ms (10.03%), Memory - 16.6 MB (100.00%)

---
+++ 0136-single-number/solution.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        count = Counter(nums)
+        return min(count , key=count.get)
+        
+        
+        
