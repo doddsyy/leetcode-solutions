3e5b3b069932d1ae90076ef1472a38cc5695f6b7
doddsyy <88795984+doddsyy@users.noreply.github.com>
2022-06-24 19:37:06 +0000

    Sync LeetCode submission Runtime - 1843 ms (5.03%), Memory - 15.6 MB (100.00%)

---
+++ 0283-move-zeroes/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        for i in range(0 , len(nums)):
+            if nums[i] == 0:
+                nums.remove(nums[i])
+                nums.append(0)
+        
