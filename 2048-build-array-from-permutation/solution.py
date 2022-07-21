f31fb6f422b821bd5111403bf3610a308185d2ed
doddsyy <88795984+doddsyy@users.noreply.github.com>
2022-07-21 20:57:58 +0000

    Sync LeetCode submission Runtime - 121 ms (6.88%), Memory - 14.1 MB (100.00%)

---
+++ 2048-build-array-from-permutation/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        li = []
+        
+        for i in nums:
+            li.append(nums[i])
+            
+        return li
+        
