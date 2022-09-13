ef552a33d04cb9c04536bd459ef078a5e4128435
doddsyy <88795984+doddsyy@users.noreply.github.com>
2022-09-13 18:23:20 +0000

    Sync LeetCode submission Runtime - 89 ms (0.00%), Memory - 13.8 MB (100.00%)

---
+++ 2093-check-if-string-is-a-prefix-of-array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        a = ''
+    
+        for i in words:
+            a += i
+            if a == s:
+                return True
+            if not s[:len(a)] == a:
+                break
+                     
+        return False 
