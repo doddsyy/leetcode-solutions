ae1fe380e845e5db46ced79ced6f16f6ae92d92a
doddsyy <88795984+doddsyy@users.noreply.github.com>
2022-10-16 21:27:53 +0000

    Sync LeetCode submission Runtime - 66 ms (0.00%), Memory - 13.8 MB (100.00%)

---
+++ 1920-determine-color-of-a-chessboard-square/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        if (ord(coordinates[0]))%2 != 0:
+            if int(coordinates[1])%2 == 0:
+                return True
+            return False
+        else:
+            if int(coordinates[1])%2 != 0:
+                return True
+            return False
