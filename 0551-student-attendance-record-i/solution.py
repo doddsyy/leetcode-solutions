4f2d491bd8a2b36ec9586538b93177629d762d00
doddsyy <88795984+doddsyy@users.noreply.github.com>
2022-10-23 15:36:06 +0000

    Sync LeetCode submission Runtime - 56 ms (0.57%), Memory - 13.7 MB (100.00%)

---
+++ 0551-student-attendance-record-i/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        if s.count('A') < 2:
+            if 'LLL' not in s:
+                return True
+        return False                
+        
