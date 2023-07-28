2bffdd86d5d37464cf56e1291ed21a4d3d65be10
doddsyy <88795984+doddsyy@users.noreply.github.com>
2023-07-28 20:56:27 +0000

    Sync LeetCode submission Runtime - 40 ms (0.00%), Memory - 16.4 MB (100.00%)

--- 2556-convert-the-temperature/solution.py
+++ 2556-convert-the-temperature/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        ans = []
-        ans.append(celsius + 273.15)
-        ans.append(celsius * 1.8 + 32)
-
-        return ans
+        return [(celsius+273.15), ((celsius*1.8)+32)]
