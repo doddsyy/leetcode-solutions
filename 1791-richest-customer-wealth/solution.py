7084c6a8f071dcd2f006664319cbb59ec4e6abb6
doddsyy <88795984+doddsyy@users.noreply.github.com>
2023-08-05 14:36:59 +0000

    Sync LeetCode submission Runtime - 47 ms (0.15%), Memory - 16.5 MB (100.00%)

--- 1791-richest-customer-wealth/solution.py
+++ 1791-richest-customer-wealth/solution.py
@@ -1,8 +1,4 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        sums = []
-        
-        for i in accounts:
-            sums.append(sum(i))
-        return max(sums)
-        
+        return max([sum(x) for x in accounts])
+                
