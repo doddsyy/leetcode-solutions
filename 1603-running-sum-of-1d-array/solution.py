8c28fd88d1dd1f90e9acc0f41f755905c9df39fa
doddsyy <88795984+doddsyy@users.noreply.github.com>
2024-07-28 10:18:52 +0000

    Sync LeetCode submission Runtime - 31 ms (5.06%), Memory - 16.6 MB (100.00%)

--- 1603-running-sum-of-1d-array/solution.py
+++ 1603-running-sum-of-1d-array/solution.py
@@ -1,10 +1,11 @@
-
-
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         
-        li = []
-        for i in range(len(nums)):
-            li.append(sum(nums[:i+1]))
+        res = []
+        sum = 0
+        for num in nums:
+            sum+=num
+            res.append(sum)
+        return res
 
-        return li
+        
