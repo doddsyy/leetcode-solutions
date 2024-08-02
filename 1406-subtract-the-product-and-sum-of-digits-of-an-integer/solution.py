70901d5025fb9be62aff4550c00eaf2cb7a62d02
doddsyy <88795984+doddsyy@users.noreply.github.com>
2024-08-02 09:57:25 +0000

    Sync LeetCode submission Runtime - 30 ms (0.03%), Memory - 16.6 MB (100.00%)

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/solution.py
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/solution.py
@@ -1,11 +1,5 @@
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
-        product = 1
-        sum = 0
-        arr = list(map(int,[x for x in str(n)]))
-        for x in arr:
-            product *= x
-            sum+=x
-        return product - sum
-        
+        li = [int(num) for num in str(n)]
+        return reduce(lambda x,y: x*y, li) - sum(li) 
         
