21fbf1043ee5d647812e3b0c95a61b6a420e70cc
doddsyy <88795984+doddsyy@users.noreply.github.com>
2024-08-05 10:44:50 +0000

    Sync LeetCode submission Runtime - 34 ms (0.26%), Memory - 16.5 MB (100.00%)

--- 2128-reverse-prefix-of-word/solution.py
+++ 2128-reverse-prefix-of-word/solution.py
@@ -1,7 +1,8 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        
         if ch in word:
             idx = word.index(ch)
-            return word[idx::-1] + word[(idx+1):]
-        else:
-            return word
+            return word[:idx+1][::-1] + word[idx+1:]
+        return word
+         
