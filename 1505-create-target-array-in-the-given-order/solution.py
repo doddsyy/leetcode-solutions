f897af840525afcd3ca187a3ee30717b52576747
doddsyy <88795984+doddsyy@users.noreply.github.com>
2024-08-07 08:32:34 +0000

    Sync LeetCode submission Runtime - 36 ms (0.26%), Memory - 16.5 MB (100.00%)

--- 1505-create-target-array-in-the-given-order/solution.py
+++ 1505-create-target-array-in-the-given-order/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
-        li=[]
-        for x in range(len(nums)):
-            li.insert(index[x],nums[x])
-            
-        return li
+        arr = []
+        for i in range(len(nums)):
+            arr.insert(index[i], nums[i])
+        return arr
