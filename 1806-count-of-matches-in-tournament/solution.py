baccfb531a1eff8ce43e96104253144aa23fa5d8
doddsyy <88795984+doddsyy@users.noreply.github.com>
2024-08-14 08:44:34 +0000

    Sync LeetCode submission Runtime - 33 ms (97.00%), Memory - 16.5 MB (100.00%)

--- 1806-count-of-matches-in-tournament/solution.py
+++ 1806-count-of-matches-in-tournament/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         matches = 0
-        
-        while n!=1:
+        while n != 1:
             if n%2 == 0:
-                n /=2
+                print(n)
+                n = n/2
                 matches+=n
-                
             else:
-                n = (n-1)/2
-                matches+=n
-                n+=1
+                matches+=((n-1)/2)
+                n = (n+1)/2
         return int(matches)
+
         
