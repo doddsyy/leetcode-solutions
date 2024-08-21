7c252959ed80b47c577b47d021630561c0d5ed62
doddsyy <88795984+doddsyy@users.noreply.github.com>
2024-08-21 09:10:23 +0000

    Sync LeetCode submission Runtime - 100 ms (19.84%), Memory - 17.1 MB (100.00%)

--- 2502-sort-the-people/solution.py
+++ 2502-sort-the-people/solution.py
@@ -1,13 +1,5 @@
-
-
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        dic = {}
-        for i in range(len(heights)):
-            dic[i] = heights[i]
-        srted = sorted(dic,key = dic.get,reverse=True)
-        li = []
-        for num in srted:
-            li.append(names[num])
-        return li
-
+        zipped = {heights[i]: names[i] for i in range(len(heights))}
+        return [value for (key, value) in sorted(zipped.items(), reverse = True)]
+        
